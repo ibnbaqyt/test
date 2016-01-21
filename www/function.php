da8201ccb9b37980022a6746d623e89daf7cbbdf
ibnbaqyt <ibnbaqyt@gmail.com>
2016-01-21 23:12:20 +0500

    Добавили еще function.php

---
+++ www/function.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Home
+ * Date: 21.01.2016
+ * Time: 23:09
+ */
---
+++ www/index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Home
+ * Date: 21.01.2016
+ * Time: 21:11
+ */
+phpinfo();
